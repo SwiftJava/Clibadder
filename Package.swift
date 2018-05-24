e263f46164e6e26c103771430f1162af873f3047
John Holdsworth <github@johnholdsworth.com>
2018-05-24 19:13:51 +0100

    V4 Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Clibadder",
+    products: [
+        .library(name: "Clibadder", targets: ["Clibadder"])
+    ],
+    dependencies: [],
     targets: [
-        Target(name: "Clibadder", dependencies: ["libadder"])
+        .target(name: "libadder"),
+        .target(name: "Clibadder", dependencies: ["libadder"]),
     ]
 )
